8f4f15582b0ed1aa039aba638c4106d8360b627e
Jeremy <cream@sourcre.am>
2019-06-07 16:35:59 -0700

    added isvalid

--- src/haxe/perdita/model/Textfield.hx
+++ src/haxe/perdita/model/Textfield.hx
@@ -4,10 +4,12 @@ class Textfield
 {
 	public var label :String;
 	public var value :String;
+	public var isValid :Bool;
 
-	public function new(label :String, value :String) : Void
+	public function new(label :String, value :String, isValid :Bool) : Void
 	{
 		this.label = label;
 		this.value = value;
+		this.isValid = isValid;
 	}
 }
